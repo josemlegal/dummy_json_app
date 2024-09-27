a70dbb03c20562afe8bfeaec6614adb6efc3d093
josemlegal <josemlegal97@gmail.com>
2024-09-27 16:39:56 -0400

    Add Shared Pref service

---
+++ lib/core/services/shared_preferences_service.dart
@@ -0,0 +1,27 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SharedPreferencesService {
+  SharedPreferences? _prefs;
+  final String _key = "savedPokemons";
+
+  SharedPreferencesService._internal();
+
+  factory SharedPreferencesService() {
+    return SharedPreferencesService._internal();
+  }
+
+  Future<SharedPreferences?> get _initializePrefs async {
+    _prefs ??= await SharedPreferences.getInstance();
+    return _prefs;
+  }
+
+  Future<List<String>> getFavoritesProducts() async {
+    var pref = await _initializePrefs;
+    return pref?.getStringList(_key) ?? [];
+  }
+
+  Future<void> setFavoritesProducts(List<String> value) async {
+    var pref = await _initializePrefs;
+    await pref?.setStringList(_key, value);
+  }
+}
